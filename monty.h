b404a6767d9a9969cd1fcb1bdf5a9d1e7b073a1b
Adedayo Akinade <akinadeadedayo1@gmail.com>
2023-06-22 14:02:57 -0700

    Modified monty.h file

--- monty.h
+++ monty.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 extern int element;
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
